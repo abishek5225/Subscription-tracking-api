8923d0b7f08ab61a6687fb053804c73b8bd71607
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-02-17 00:06:07 +0545

    updated

--- server/models/subscription.model.js
+++ server/models/subscription.model.js
@@ -14,7 +14,15 @@ const userSchema = new mongoose.Schema({
         unique: true,
         trim: true,
         lowercase: true,
-        match : [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "Please fill a valid email address"],
-             
+        match : [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "Please fill a valid email address"],      
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minLength: 8,
     }
-})
+}, {timestamps: true});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
