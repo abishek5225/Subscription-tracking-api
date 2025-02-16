405306952d2de7e81a5b35da43bdc55b75f6325e
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-02-16 20:38:27 +0545

    updated

--- server/app.js
+++ server/app.js
@@ -6,6 +6,9 @@ import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 
+//database
+import connectToDatabase from './database/mongodb.js';
+
 const app = express();
 
 app.use(express.json());
@@ -19,8 +22,10 @@ app.get('/', (req, res) => {
     res.send('welcome to the subscription tracker API');
 })
 
-app.listen(PORT , () => {
+app.listen(PORT ,async () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+
+    await connectToDatabase();
 })
 
 export default app;
