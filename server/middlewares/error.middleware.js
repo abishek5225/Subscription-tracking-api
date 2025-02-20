9f3ca14414475b1bb35d1543e11bb831d3529d9e
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-02-21 00:18:10 +0545

    updated

--- server/middlewares/error.middleware.js
+++ server/middlewares/error.middleware.js
@@ -1,5 +1,31 @@
 
 
 const errorMIddleware = (err, req, res, next)=>{
+    try{
+        let error = {...err}
+
+        error.message = err.message;
+
+        console.error(err);
+        
+        //mongoose bad objectID error
+        if(err.name === 'CastError'){
+            const messsage = `Resource not found`
+            error = new Error(messsage);
+            error.status = 404;
+        }
+
+        //mongoose duplicate key
+        if(err.code === 11000){
+           const message = 'Duplicate field value entered';
+           error = new Error(message);
+           error.status = 400;
+        }
+
+        //mongoose validation error
+        
+    }catch(error){
+        next(error)
+    }
 
 }
