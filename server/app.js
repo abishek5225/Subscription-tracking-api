8e50d2635b82f91197e6531150cd13d8804b1b61
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-02-21 00:38:50 +0545

    updated

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import {PORT} from './config/env.js'
+import cookieParser from 'cookie-parser'
 
 //importing routes
 import userRouter from './routes/user.routes.js';
@@ -8,15 +9,21 @@ import subscriptionRouter from './routes/subscription.routes.js';
 
 //database
 import connectToDatabase from './database/mongodb.js';
+import errorMIddleware from './middlewares/error.middleware.js';
 
 const app = express();
 
+//builtin middlewares
 app.use(express.json());
-
+app.use(express.urlencoded({extended: false})); // this helps us to process the form data sent via html forms in a simple format
+app.use(cookieParser())
 //routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+//errormiddleware
+app.use(errorMIddleware)
 
 app.get('/', (req, res) => {
     res.send('welcome to the subscription tracker API');
