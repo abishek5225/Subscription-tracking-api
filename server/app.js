6b91cc32acc4e1aeef0907c8f835cdf32dce540e
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-02-21 18:53:29 +0545

    updated

--- server/app.js
+++ server/app.js
@@ -17,6 +17,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false})); // this helps us to process the form data sent via html forms in a simple format
 app.use(cookieParser())
+
+
 //routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
